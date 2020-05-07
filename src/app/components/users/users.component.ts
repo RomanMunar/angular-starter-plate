17663735e0871559572e7c5c5ceb21e08a254351
RomanMunar <roman.munar1@gmail.com>
2020-05-07 10:00:19 +0800

    added 'data.service' with 'getUsers' observables

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../models/User';
+import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -15,56 +16,27 @@ export class UsersComponent implements OnInit {
   users: User[];
   showExtended: boolean = true;
   enableAdd: boolean = true;
+  loaded: boolean;
   showUserForm: boolean = false;
   @ViewChild('userForm') form: any;
-  constructor() {}
+  data: any;
+
+  constructor(private _dataService: DataService) {}
 
   ngOnInit(): void {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'John@yahoo.com',
-        isActive: false,
-        registered: new Date('01/02/2018 08:45:00'),
-        hide: false,
-      },
-      {
-        firstName: 'Roman',
-        lastName: 'Munar',
-        email: 'RomanMunar@gmail.com',
-        isActive: true,
-        registered: new Date('04/07/2018 08:45:00'),
-        hide: false,
-      },
-      {
-        firstName: 'Dohn',
-        lastName: 'Joe',
-        email: 'Dohn@eme.er',
-        isActive: true,
-        registered: new Date('01/025/2018 08:45:00'),
-        hide: false,
-      },
-    ];
+    this._dataService.getUsers().subscribe((users) => {
+      this.users = users;
+      this.loaded = true;
+    });
   }
-  // addUser() {
-  //   this.user.isActive = true;
-  //   this.user.registered = new Date();
 
-  //   this.users.unshift(this.user);
-  //   this.user = {
-  //     firstName: '',
-  //     lastName: '',
-  //     email: '',
-  //   };
-  // }
   onSubmit({ value, valid }: { value: User; valid: boolean }) {
     if (!valid) {
       console.log('Form is not valid');
     } else {
       value.isActive = true;
       value.registered = new Date();
-      this.users.unshift(value);
+      this._dataService.addUser(value);
       this.form.reset();
     }
   }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { User } from '../models/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  users: User[];
+  constructor() {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'John@yahoo.com',
+        isActive: false,
+        registered: new Date('01/02/2018 08:45:00'),
+        hide: false,
+      },
+      {
+        firstName: 'Roman',
+        lastName: 'Munar',
+        email: 'RomanMunar@gmail.com',
+        isActive: true,
+        registered: new Date('04/07/2018 08:45:00'),
+        hide: false,
+      },
+      {
+        firstName: 'Dohn',
+        lastName: 'Joe',
+        email: 'Dohn@eme.er',
+        isActive: true,
+        registered: new Date('01/025/2018 08:45:00'),
+        hide: false,
+      },
+    ];
+  }
+
+  getUsers(): Observable<User[]> {
+    return of(this.users);
+  }
+
+  addUser(user: User) {
+    this.users.unshift(user);
+  }
+}
